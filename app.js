24d9f8013ebe31bc6322cda69feb38b34c156441
NightStrang6r <47156355+NightStrang6r@users.noreply.github.com>
2022-01-02 00:32:29 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const {app, BrowserWindow} = require('electron');
+const config = require('./config.json');
+
+let window = null;
+
+app.once('ready', () => {
+  window = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    show: false,
+    icon: __dirname + 'img/logo.png',
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  });
+
+  window.webContents.session
+    .setProxy({proxyRules: "http://leoitdev.ru:3128"})
+    .then(
+      () => {
+        window.loadURL('https://vk.com/');
+      }
+    )
+    .catch((err) => console.error(err));
+
+  window.once('ready-to-show', () => {
+    window.show();
+  });
+});
+
+app.on('login', (event, webContents, details, authInfo, callback) => {
+  console.log("Loading proxy login...");
+  event.preventDefault();
+  callback(config.proxyLogin, config.proxyPass);
+  console.log("Proxy loaded. Starting window.");
+});
